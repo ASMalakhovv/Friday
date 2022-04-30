a1d6ba652aa2311b4a4fcceadce081a65da70c4b
Red-K4tana <ceptor.nill@gmail.com>
2022-04-30 16:34:04 +0300

    test commit, begin operation!!!

--- src/ui/features/auth/login/Login.tsx
+++ src/ui/features/auth/login/Login.tsx
@@ -1,11 +1,29 @@
 import s from './Login.module.scss'
+import SuperCheckbox from "../../../common/SuperCheckbox/SuperCheckbox";
+import SuperButton from "../../../common/SuperButton/SuperButton";
+import {NavLink} from "react-router-dom";
+import {path} from "../../../main/routes/Pages";
+import SuperInputText from "../../../common/SuperInputText/SuperInputText";
 
 
 export function Login() {
     return (
         <div className={s.loginBlock}>
             <div className={s.loginContainer}>
-                Login
+                <div>
+                    It-incubator
+                    Sign In
+                </div>
+                <div>
+                    <SuperInputText/>
+                    <SuperInputText/>
+                    <SuperCheckbox/>
+                </div>
+                <div>
+                    <SuperButton/>
+                    Don`t have an account?
+                    <NavLink to={path.signup}>Sign Up</NavLink>
+                </div>
             </div>
         </div>
     );
