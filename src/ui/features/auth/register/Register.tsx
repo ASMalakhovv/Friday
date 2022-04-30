2e7768386fbffd6dcc7c7e0a7abeb9370515e67e
ASMalakhovv <alsmalahovv@mail.ru>
2022-04-30 22:22:27 +0600

    Made: jsx for the component Register.tsx Change: Main.module.scss made width 100%

--- src/ui/features/auth/register/Register.tsx
+++ src/ui/features/auth/register/Register.tsx
@@ -1,10 +1,22 @@
 import s from './Register.module.scss'
+import SuperInputText from "../../../common/SuperInputText/SuperInputText";
+import SuperButton from "../../../common/SuperButton/SuperButton";
 
 export function Register() {
     return (
         <div className={s.registerContainer}>
             <div className={s.registerBlock}>
-
+                <h3>it-incubator</h3>
+                <h3>Sign Up</h3>
+                <SuperInputText/>
+                <SuperInputText/>
+                <SuperInputText/>
+                <SuperButton>
+                    Cancel
+                </SuperButton>
+                <SuperButton>
+                    Register
+                </SuperButton>
             </div>
         </div>
     );
