02ad688be64d98bb9b31f77d6592434987610286
ASMalakhovv <alsmalahovv@mail.ru>
2022-05-05 20:31:53 +0600

    refactoring

--- src/ui/features/auth/login/Login.tsx
+++ src/ui/features/auth/login/Login.tsx
@@ -5,7 +5,7 @@ export function Login() {
     return (
         <div className={s.loginBlock}>
             <div className={s.loginContainer}>
-                Login---dwadadawddwdddd
+                Login---dwadadawddw
             </div>
         </div>
     );
