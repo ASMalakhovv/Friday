713d128eee92ef68f62c486082ac1ae5d7057fd4
blinkkk <58583448+bair-182@users.noreply.github.com>
2022-05-06 23:49:11 +0900

    + (Unfinished) created editing profile container

--- src/ui/features/profile/Profile.tsx
+++ src/ui/features/profile/Profile.tsx
@@ -7,8 +7,7 @@ import s from "../auth/login/Login.module.scss";
 import {ProgressBar} from "../../components/ProgressBar/ProgressBar";
 import PopUpWindowRegistration from "../../components/PopUpWindow/PopUpWindowRegistration/PopUpWindowRegistration";
 import {setErrorLogin} from "../auth/login/login-reducer";
-
-
+import {red} from "@mui/material/colors";
 
 
 export function Profile() {
@@ -19,14 +18,17 @@ export function Profile() {
     const profileInfo = useAppSelector(state => state.profile)
     const error: null | string = useAppSelector(state => state.login.error)
 
+    const classNameInput = error ? `${s.error}` : ''
+
     const [ProfileEditMode, setProfileEditMode] = useState<boolean>(false)
+    const [] = useState('')
 
     const closePopUpWindow = useCallback(() => {
         error && dispatch(setErrorLogin(null))
     }, [error])
 
-    const ChangeProfileEditModeHandler =()=> {
-        setProfileEditMode(true)
+    const ChangeProfileEditModeHandler = () => {
+        setProfileEditMode(!ProfileEditMode)
     }
 
     if (ProfileEditMode) {
@@ -53,7 +55,7 @@ export function Profile() {
 
                             <header className={s.header}>
                                 <h2>Personal information</h2>
-                                <img src={'https://avatarko.ru/img/avatar/2/zhivotnye_igra_kot_1816.jpg'} />
+                                <img src={'https://avatarko.ru/img/avatar/2/zhivotnye_igra_kot_1816.jpg'}/>
                                 <p>ava URL: {profileInfo.avatar}</p>
                             </header>
                             <div className={s.header}>
@@ -62,9 +64,33 @@ export function Profile() {
                             <div className={s.header}>
                                 <p>e-mail: {profileInfo.email}</p>
                             </div>
+
+                            <div className={s.inputContainer}>
+                                <label>Email</label>
+                                <input type='textasd'
+                                       value={'valueEmail'}
+                                       onChange={()=>{}}
+                                       data-input='email'
+                                       autoComplete="off"
+                                       className={classNameInput}
+                                />
+                            </div>
+
+                            <span>
+                                <SuperButton onClick={ChangeProfileEditModeHandler}>
+                                CANCEL
+                                </SuperButton>
+                            </span>
+                            <span>
+                                <SuperButton className='default' red={true}>
+                                SAVE
+                                </SuperButton>
+                            </span>
+
                         </div>
 
                     </div>
+
 
                 </div>
             </div>
@@ -76,44 +102,44 @@ export function Profile() {
             ?
             <Navigate to={path.login}/>
             :
-        <div>
             <div>
-                Profile INFO
-            </div>
-
-            <div className={s.popUpContainer}>
-                <div className={s.popUp}>
-                    {error && <PopUpWindowRegistration value={error} callback={closePopUpWindow}
-                                                       className={s.popUp}
-                                                       header='Error'
-                    />}
-                </div>
-            </div>
-            <div className={s.loginBlock}>
-                <div className={s.loginContainer}>
-                    <div className={s.progressBar}>
-                        {isLoading && <ProgressBar/>}
-                    </div>
-
-                    <header className={s.header}>
-                        <h2>Personal information</h2>
-                        <img src={'https://avatarko.ru/img/avatar/2/zhivotnye_igra_kot_1816.jpg'} />
-                        <p>ava URL: {profileInfo.avatar}</p>
-                    </header>
-                    <div className={s.header}>
-                        <h3>{profileInfo.name}</h3>
-                    </div>
-                    <div className={s.header}>
-                        <p>e-mail: {profileInfo.email}</p>
-                    </div>
-                    <SuperButton onClick={ChangeProfileEditModeHandler}>
-                        Edit profile
-                    </SuperButton>
+                <div>
+                    Profile INFO
                 </div>
 
-            </div>
+                <div className={s.popUpContainer}>
+                    <div className={s.popUp}>
+                        {error && <PopUpWindowRegistration value={error} callback={closePopUpWindow}
+                                                           className={s.popUp}
+                                                           header='Error'
+                        />}
+                    </div>
+                </div>
+                <div className={s.loginBlock}>
+                    <div className={s.loginContainer}>
+                        <div className={s.progressBar}>
+                            {isLoading && <ProgressBar/>}
+                        </div>
 
-        </div>
+                        <header className={s.header}>
+                            <h2>Personal information</h2>
+                            <img src={'https://avatarko.ru/img/avatar/2/zhivotnye_igra_kot_1816.jpg'}/>
+                            <p>ava URL: {profileInfo.avatar}</p>
+                        </header>
+                        <div className={s.header}>
+                            <h3>{profileInfo.name}</h3>
+                        </div>
+                        <div className={s.header}>
+                            <p>e-mail: {profileInfo.email}</p>
+                        </div>
+                        <SuperButton onClick={ChangeProfileEditModeHandler}>
+                            Edit profile
+                        </SuperButton>
+                    </div>
+
+                </div>
+
+            </div>
     )
 }
 
