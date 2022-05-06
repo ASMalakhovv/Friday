b283225e9985df7236b0c747e34efce0eeb83bb9
blinkkk <58583448+bair-182@users.noreply.github.com>
2022-05-06 22:46:08 +0900

    + Created profile component + Partly created editing profile component + added to api: authMe, logOut,

--- src/ui/features/profile/Profile.tsx
+++ src/ui/features/profile/Profile.tsx
@@ -1,7 +1,7 @@
 import {useAppDispatch, useAppSelector} from "../../../hooks/useReactRedux";
 import {Navigate} from "react-router-dom";
 import {path} from "../../main/routes/Pages";
-import React, {useCallback} from "react";
+import React, {useCallback, useState} from "react";
 import SuperButton from "../../common/SuperButton/SuperButton";
 import s from "../auth/login/Login.module.scss";
 import {ProgressBar} from "../../components/ProgressBar/ProgressBar";
@@ -19,10 +19,57 @@ export function Profile() {
     const profileInfo = useAppSelector(state => state.profile)
     const error: null | string = useAppSelector(state => state.login.error)
 
+    const [ProfileEditMode, setProfileEditMode] = useState<boolean>(false)
 
     const closePopUpWindow = useCallback(() => {
         error && dispatch(setErrorLogin(null))
     }, [error])
+
+    const ChangeProfileEditModeHandler =()=> {
+        setProfileEditMode(true)
+    }
+
+    if (ProfileEditMode) {
+        return (
+            <div>
+                <div>
+                    <div>
+                        EDIT PROFILE MODE
+                    </div>
+
+                    <div className={s.popUpContainer}>
+                        <div className={s.popUp}>
+                            {error && <PopUpWindowRegistration value={error} callback={closePopUpWindow}
+                                                               className={s.popUp}
+                                                               header='Error'
+                            />}
+                        </div>
+                    </div>
+                    <div className={s.loginBlock}>
+                        <div className={s.loginContainer}>
+                            <div className={s.progressBar}>
+                                {isLoading && <ProgressBar/>}
+                            </div>
+
+                            <header className={s.header}>
+                                <h2>Personal information</h2>
+                                <img src={'https://avatarko.ru/img/avatar/2/zhivotnye_igra_kot_1816.jpg'} />
+                                <p>ava URL: {profileInfo.avatar}</p>
+                            </header>
+                            <div className={s.header}>
+                                <h3>{profileInfo.name}</h3>
+                            </div>
+                            <div className={s.header}>
+                                <p>e-mail: {profileInfo.email}</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
 
     return (
         !profileId
@@ -59,7 +106,7 @@ export function Profile() {
                     <div className={s.header}>
                         <p>e-mail: {profileInfo.email}</p>
                     </div>
-                    <SuperButton>
+                    <SuperButton onClick={ChangeProfileEditModeHandler}>
                         Edit profile
                     </SuperButton>
                 </div>
